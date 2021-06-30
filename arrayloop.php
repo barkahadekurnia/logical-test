ea93c2046139d2516b2537e14ffdcff9c4af33d7
adevanjava <barkahadekurnia@gmail.com>
2021-06-30 21:47:59 +0700

    no1 array loop

---
+++ arrayloop.php
@@ -0,0 +1,19 @@
+<?php
+//barkahadekurnia@gmail.com 
+
+//isisialisasi disit datane
+$aplikasi = [];
+$aplikasi[1] = "gtAkademik";
+$aplikasi[2] = "gtFinansi";
+$aplikasi[3] = "gtPerizinan";
+$aplikasi[4] = "eCampuz";
+$aplikasi[5] = "eOviz";
+
+//buat loop untuk menampilkan semua data diatas
+
+
+$i = 1;
+while ($i <= 5) {
+    echo "$aplikasi[$i] \n";
+    $i++;
+}
